e45c1f3160c57610081030ad672662528323b9ae
Anna Strik <anna.strik1@gmail.com>
2021-03-21 02:12:36 -0700

    filtering parameters moved to separate file assert.js

---
+++ tests/fixtures/assert.js
@@ -0,0 +1,7 @@
+const filter = {
+  sportType: 'CYCLING',
+  intensity: '2',
+  startTime: ['morning', 5, 12, 'AM'],
+};
+
+export { filter };
--- tests/specs/LoadAndFilter/FilterEvents.spec.js
+++ tests/specs/LoadAndFilter/FilterEvents.spec.js
@@ -3,44 +3,32 @@ import HomePage from '../../pages/HomePage';
 import EventsPage from '../../pages/EventsPage';
 import NavigMenu from '../../pages/NavigMenu';
 import helper from '../../helper.js';
-
-const sportType = 'CYCLING';
-const intensity = '2';
-const startTime = ['morning', 5, 12, 'AM'];
-let eventsCountBeforeFilter;
+import { filter } from '../../fixtures/assert.js';
 
 describe('FILTER EVENTS', () => {
-  before('should open events page', () => {
+  before('should open events page and validate the page is loaded', () => {
     HomePage.open();
     helper.waitForPageLoaded();
     HomePage.openSideNavMenu();
     NavigMenu.openEventsPage();
-  });
-
-  it('should validate the events page is loaded', () => {
     helper.waitForPageLoaded();
   });
 
-  it('should count events and record the full events headers list', () => {
-    eventsCountBeforeFilter = EventsPage.eventsCount;
-  });
-
-  it('should apply filter on sports type, intensities and start times', () => {
-    EventsPage.filterSportIntensTimes(sportType, intensity, startTime[0]);
-  });
-
   it('should validate that events list has changed after filtering', () => {
+    const eventsCountBeforeFilter = EventsPage.eventsCount;
+    EventsPage.filterSportIntensTimes(filter.sportType, filter.intensity, filter.startTime[0]);
+
     expect(eventsCountBeforeFilter).not.eq(EventsPage.eventsCount);
   });
 
   it('should validate that events list meet the filtering criteria and system behaves correctly when list is empty', () => {
     if (EventsPage.eventsListNotEmpty()){
       expect(EventsPage.eventsCount).eq(
-        EventsPage.getEventSportTypeList.filter(el => el === sportType).length);
+        EventsPage.getEventSportTypeList.filter(el => el === filter.sportType).length);
       expect(EventsPage.eventsCount).eq(
-        EventsPage.getIntensityList.filter(el => el === intensity).length);
+        EventsPage.getIntensityList.filter(el => el === filter.intensity).length);
       expect(EventsPage.eventsCount).eq(EventsPage.getEventTimeList.filter(
-        el => el[0] >= startTime[1] && el[0] < startTime[2] && el[1].slice(-2) === startTime[3]).length);
+        el => el[0] >= filter.startTime[1] && el[0] < filter.startTime[2] && el[1].slice(-2) === filter.startTime[3]).length);
     } else {
       expect(EventsPage.noResultsNote.isDisplayed()).true;
     }
